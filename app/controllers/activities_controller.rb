491459050aa58b908a313191367b44f1017853aa
Pete Fowler <fowler.pete@gmail.com>
2022-10-19 12:23:16 -0600

    Pass activities tests

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,2 +1,12 @@
 class ActivitiesController < ApplicationController
+  def index 
+    act = Activity.all
+    render json: act
+  end
+
+  def destroy
+    act = Activity.find(params[:id])
+    act.destroy
+    render json: {}, status: :accepted
+  end
 end
