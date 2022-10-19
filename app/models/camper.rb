477ba0553a94ce556cb18557ea226c58e4711602
Pete Fowler <fowler.pete@gmail.com>
2022-10-19 11:56:16 -0600

    Fix camper macro

--- app/models/camper.rb
+++ app/models/camper.rb
@@ -1,6 +1,6 @@
 class Camper < ApplicationRecord
   has_many :signups
-  has_many :activies, through: :signups
+  has_many :activities, through: :signups
   validates :name, presence: true 
   validates :age, presence: true, numericality: {greater_than: 7, less_than: 19}
 end
