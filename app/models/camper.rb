80660e50e15c78e2b1149b78c8ff67cb6de06c44
Pete Fowler <fowler.pete@gmail.com>
2022-10-19 11:42:27 -0600

    Add validations

--- app/models/camper.rb
+++ app/models/camper.rb
@@ -1,5 +1,6 @@
 class Camper < ApplicationRecord
   has_many :signups
   has_many :activies, through: :signups
-
+  validates :name, presence: true 
+  validates :age, presence: true, numericality: {greater_than: 7, less_than: 19}
 end
--- app/models/signup.rb
+++ app/models/signup.rb
@@ -1,4 +1,5 @@
 class Signup < ApplicationRecord
   belongs_to :activity
   belongs_to :camper
+  validates :time, presence: true, numericality: {greater_than_or_equal_to: 0, less_than_or_equal_to: 23}
 end
